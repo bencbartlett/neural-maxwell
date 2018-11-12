2ed38ef4f19a5b45244509a3e3cef57a2ea674dc
Ben Bartlett <benjamincbartlett@gmail.com>
2018-11-11 18:44:09 -0800

    Rewrite convolutional model in pytorch; shit still don't work

---
+++ models/utils.py
@@ -0,0 +1,17 @@
+from tqdm import tqdm, tqdm_notebook
+
+def is_notebook():
+    '''Tests to see if we are running in a jupyter notebook environment'''
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True  # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
+pbar = tqdm_notebook if is_notebook() else tqdm
